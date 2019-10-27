8e58b9d93778fbc1a373ef4d153b07e571154533
Zoltan Fazekas <ongroa@gmail.com>
2019-10-27 06:22:15 +0100

    add automatic size

--- game_of_life.cc
+++ game_of_life.cc
@@ -1,8 +1,12 @@
+#include <sys/ioctl.h>
+#include <unistd.h>
+
 #include <chrono>
 #include <cstdio>
 #include <ctime>
 #include <iostream>
 #include <random>
+#include <set>
 #include <thread>
 #include <vector>
 
@@ -12,17 +16,23 @@ using namespace std;
 
 class GameOfLife {
   public:
-    GameOfLife() {
-        _original_density = rand() % 100;
-        for (unsigned i = 1; i <= Y; ++i) {
-            for (unsigned j = 1; j <= X; ++j) {
+    GameOfLife(const int x_, const int y_, const int original_density_) :
+    x{x_}, y{y_}, _original_density{original_density_} {
+        for (int i = 0; i < y; ++i) {
+            vector<bool> v;
+            for (int j = 0; j < x; ++j) {
                 if ((rand() % 100) < _original_density) {
-                    _table[i][j] = true;
+                    v.push_back(true);
+                    ++_living_cells;
                 } else {
-                    _table[i][j] = false;
+                    v.push_back(false);
                 }
             }
+            _table.push_back(v);
+            _next_table.push_back(v);
         }
+        set_original_density();
+        set_densities();
     }
 
     int get_original_density() const {
@@ -33,97 +43,94 @@ class GameOfLife {
         return _generations;
     }
 
-    unsigned get_current_density() const {
+    int get_current_density() const {
         return _current_density;
     }
 
-    unsigned get_min_density() const {
+    int get_min_density() const {
         return _min_density;
     }
 
-    unsigned get_max_density() const {
+    int get_max_density() const {
         return _max_density;
     }
 
-    int get_x() const {
-        return X;
-    }
-
-    int get_y() const {
-        return Y;
-    }
-
     void generation() {
+        static set<int> staying_alive{2, 3};
+        static set<int> born{3};
         _living_cells = 0;
         ++_generations;
-        bool next_table[Y + 2][X + 2]{false};
-        for (unsigned i = 1; i <= Y; ++i) {
-            for (unsigned j = 1; j <= X; ++j) {
-                const unsigned number_of_adj = get_number_of_adj(i, j);
+        for (int i = 0; i < y; ++i) {
+            for (int j = 0; j < x; ++j) {
+                const int number_of_adj = get_number_of_adj(i, j);
                 if (_table[i][j]) {
-                    if (number_of_adj == 2 || number_of_adj == 3) {
-                        next_table[i][j] = true;
+                    if (staying_alive.find(number_of_adj) != staying_alive.end()) {
+                        _next_table[i][j] = true;
                         ++_living_cells;
                     } else {
-                        next_table[i][j] = false;
+                        _next_table[i][j] = false;
                     }
                 } else {
-                    if (number_of_adj == 3) {
-                        next_table[i][j] = true;
+                    if (born.find(number_of_adj) != born.end()) {
+                        _next_table[i][j] = true;
                         ++_living_cells;
                     } else {
-                        next_table[i][j] = false;
+                        _next_table[i][j] = false;
                     }
                 }
             }
         }
-        _current_density = 100 * _living_cells / X / Y;
+        swap(_table, _next_table);
+        set_densities();
+    }
+
+    friend ostream& operator<<(ostream& os, const GameOfLife& g);
+
+  private:
+    int x;
+    int y;
+    int _original_density;
+    int _current_density;
+    int _min_density{100};
+    int _max_density{0};
+    int _living_cells{0};
+    int long _generations{0};
+    vector<vector<bool>> _table;
+    vector<vector<bool>> _next_table;
+
+    void set_original_density() {
+        _original_density = 100 * _living_cells / x / y;
+    }
+
+    void set_densities() {
+        _current_density = 100 * _living_cells / x / y;
         if (_current_density < _min_density) {
             _min_density = _current_density;
         }
         if (_current_density > _max_density) {
             _max_density = _current_density;
         }
-        for (unsigned i = 1; i <= Y; ++i) {
-            for (unsigned j = 1; j <= X; ++j) {
-                _table[i][j] = next_table[i][j];
-            }
-        }
     }
 
-    friend ostream& operator<<(ostream& os, const GameOfLife& g);
-
-  private:
-    static constexpr int X = 200;
-    static constexpr int Y = 48;
-
-    int _original_density;
-    int _current_density;
-    int _min_density{100};
-    int _max_density{0};
-    unsigned _living_cells{0};
-    unsigned long _generations = 0;
-    bool _table[Y + 2][X + 2]{false};
-
-    unsigned get_number_of_adj(const int i, const int j) {
-        unsigned sum = 0;
-        if (_table[i - 1][j - 1]) ++sum;
-        if (_table[i - 1][j    ]) ++sum;
-        if (_table[i - 1][j + 1]) ++sum;
-        if (_table[i    ][j - 1]) ++sum;
-        if (_table[i    ][j + 1]) ++sum;
-        if (_table[i + 1][j - 1]) ++sum;
-        if (_table[i + 1][j    ]) ++sum;
-        if (_table[i + 1][j + 1]) ++sum;
+    int get_number_of_adj(const int i, const int j) {
+        int sum = 0;
+        if (i > 0 && j > 0 && _table[i - 1][j - 1]) ++sum;
+        if (i > 0 && _table[i - 1][j]) ++sum;
+        if (i > 0 && j < x - 1 && _table[i - 1][j + 1]) ++sum;
+        if (j > 0 && _table[i][j - 1]) ++sum;
+        if (j < x - 1 && _table[i][j + 1]) ++sum;
+        if (i < y - 1 && j > 0 && _table[i + 1][j - 1]) ++sum;
+        if (i < y - 1 && _table[i + 1][j]) ++sum;
+        if (i < y - 1 && j < x - 1 && _table[i + 1][j + 1]) ++sum;
         return sum;
     }
 };
 
 ostream& operator<<(ostream& os, const GameOfLife& g) {
     clrscr();
-    for (unsigned i = 0; i < g.Y + 2; ++i) {
-        for (unsigned j = 0; j < g.X + 2; ++j) {
-            if (i == 0 || i == g.Y + 1 || j == 0 || j == g.X + 1) {
+    for (int i = -1; i <= g.y; ++i) {
+        for (int j = -1; j <= g.x; ++j) {
+            if (i == -1 || i == g.y || j == -1 || j == g.x) {
                 os << "#";
             } else if (g._table[i][j]) {
                 os << "0";
@@ -144,20 +151,22 @@ ostream& operator<<(ostream& os, const GameOfLife& g) {
        << "%, Generations: "
        << g.get_generations()
        << endl
-       << "X: " << g.get_x()
-       << ", Y: " << g.get_y()
-       << endl;
+       << "X: " << g.x
+       << ", Y: " << g.y;
     return os;
 }
 
 void clrscr() {
-   fprintf(stdout, "\033[2J"); // clean screen
-   fprintf(stdout, "\033[1;1H"); // move cursor to the first line
+   printf("\033[2J"); // clean screen
+   printf("\033[1;1H"); // move cursor to the first line
 }
 
 int main() {
-    srand((unsigned)time(0));
-    GameOfLife game_of_life;
+    srand((int)time(0));
+    struct winsize win;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &win);
+    int original_density = rand() % 100;
+    GameOfLife game_of_life(win.ws_col - 2, win.ws_row - 5, original_density);
     cout << game_of_life << endl;
     this_thread::sleep_for(chrono::milliseconds(2000));
     for (;;) {
